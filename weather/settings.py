36cfc4eeef01b49ff2a7bac19c8ea6cc57f93944
Aaron Randall <aaronasherrandall@gmail.com>
2022-11-23 15:55:55 +0900

    settings update

--- weather/settings.py
+++ weather/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-**6aaoy=9dfbpwsw6kxs3ys8^#p73*&_^)*di)o*c0qssygp5r'
+from decouple import config
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
